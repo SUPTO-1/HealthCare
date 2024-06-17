8524683dcedf851e7f0629696667630630a7293c
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-06-17 18:32:07 +0600

    fetching data using tanstack query

--- src/Banner/BannerList.jsx
+++ src/Banner/BannerList.jsx
@@ -1,10 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "../CustomHook/UseAxiosSecure";
 
 const BannerList = () => {
     const axiosSecure = UseAxiosSecure();
+    const {data: banner = []} = useQuery({
+        queryKey:['banner'],
+        queryFn: async()=>{
+            const res = await axiosSecure.get('/banner');
+            return res.data;
+        }
+    })
     return (
         <div className="min-h-screen">
-            <h2 className="text-2xl font-roboto text-center mt-10">All Available Banners</h2>
+            <h2 className="text-2xl font-roboto text-center mt-10">All Available Banners: {banner.length} </h2>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './Routes/Route.jsx'
-import AuthProvider from './Providers/AuthProvider.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./Routes/Route.jsx";
+import AuthProvider from "./Providers/AuthProvider.jsx";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <AuthProvider>
-   <RouterProvider router={routes}></RouterProvider>
-   </AuthProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routes}></RouterProvider>
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
