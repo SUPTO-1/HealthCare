371a3e84b06b8a1c4cf2b11ccf6afe7373e553bd
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-06-18 22:19:12 +0600

    user can add test

--- src/Alltests/AddTest.jsx
+++ src/Alltests/AddTest.jsx
@@ -2,10 +2,46 @@ import DatePicker from "react-datepicker";
 import addTestImg from "../assets/images/addTest.jpg";
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
+import UseAxiosSecure from "../CustomHook/UseAxiosSecure";
+import Swal from "sweetalert2";
 const AddTest = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const axiosSecure = UseAxiosSecure();
+  const handleAdd = (e) =>{
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const testName = form.get('testName');
+    const testFee = form.get('testFee');
+    const description = form.get('description');
+    const slot = form.get('slot');
+    const date = form.get('date');
+    const image = form.get('imageURL');
+    const addTest = {
+        testName,
+        testFee,
+        description,
+        slot,
+        date,
+        image
+    }
+    //console.log(addTest);
+    axiosSecure.post('/test',addTest)
+    .then(res=>{
+        if(res.data.insertedId)
+            {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Test added successfully",
+                    icon: "success",
+                    confirmButtonText: "Okay",
+                  });
+                  e.target.reset();
+            }
+    })
+   
+  }
   return (
-    <div className=" md:mx-8 font-roboto lg:mt-10 lg:flex  gap-5 border-2 rounded-lg shadow-xl p-0 md:4 lg:p-0">
+    <div className=" md:mx-8 font-roboto lg:mt-10 lg:flex  gap-5 p-0 md:4 lg:p-0">
       <div className="flex-1 hidden lg:block md:rounded-r-[150px] rounded-r-lg ">
         <img src={addTestImg} className="object-cover w-full h-full" alt="" />
       </div>
@@ -13,7 +49,7 @@ const AddTest = () => {
         <h2 className="text-xl md:text-3xl text-center text-[#80B9AD] font-poppins md:pt-2 lg:mt-0 font-bold">
           Add Test
         </h2>
-        <form>
+        <form onSubmit={handleAdd}>
           <div className="space-y-6">
             <div>
               <label className="block text-lg font-medium text-gray-700 mb-2">
@@ -24,18 +60,6 @@ const AddTest = () => {
                 placeholder="Enter Test Name"
                 className="input block w-full px-2 py-2 lg:px-4 lg:py-3 border border-gray-300 rounded-md text-base focus:outline-none"
                 name="testName"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-lg font-medium text-gray-700 mb-2">
-                Title
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Title"
-                className="input block w-full px-2 py-2 lg:px-4 lg:py-3 border border-gray-300 rounded-md text-base focus:outline-none"
-                name="title"
                 required
               />
             </div>
@@ -57,7 +81,7 @@ const AddTest = () => {
               </label>
               <input
                 type="text"
-                placeholder="Banner URL"
+                placeholder="Image URL"
                 className="input block w-full px-2 py-2 lg:px-4 lg:py-3 border border-gray-300 rounded-md text-base focus:outline-none"
                 name="imageURL"
                 required
